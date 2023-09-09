9bd474b29ebea8bcb5a1bf002a6ebb0533ad8ed0
Toby Sullivan <tobyjsullivan@gmail.com>
2023-09-09 13:11:44 -0700

    Render UPC barcodes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import JsBarcode from "jsbarcode";
+
+import "./App.css";
+import { useCallback, useState } from "react";
+
+const DEFAULT_CODE = "628942663321";
+
+function drawCode(el: HTMLOrSVGElement, code: string): void {
+  JsBarcode(el, code, { format: "UPC" });
+}
 
 function App() {
+  const [code, setCode] = useState(DEFAULT_CODE);
+  const [error, setError] = useState<string | undefined>(undefined);
+  const [downloadUrl, setDownloadUrl] = useState<string | null>(null);
+
+  const updateRef = useCallback(
+    (el: SVGSVGElement | null) => {
+      if (el === null) {
+        return;
+      }
+      try {
+        drawCode(el, code);
+        setError(undefined);
+
+        var svgData = el.outerHTML;
+        var svgBlob = new Blob([svgData], {
+          type: "image/svg+xml;charset=utf-8",
+        });
+        var svgUrl = URL.createObjectURL(svgBlob);
+        setDownloadUrl(svgUrl);
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err.message);
+        } else {
+          setError(String(err));
+        }
+      }
+    },
+    [code]
+  );
+
+  const errMessage =
+    error === undefined ? null : <p className="error">Error: {error}</p>;
+
+  const downloadLink =
+    downloadUrl === null ? null : (
+      <a href={downloadUrl} download={"upc.svg"}>
+        Download
+      </a>
+    );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="input">
+        <input
+          type="text"
+          defaultValue={code}
+          placeholder="GTI12"
+          onChange={(e) => setCode(e.target.value)}
+        />
+        {errMessage}
+      </div>
+      <div
+        className="output"
+        style={{
+          display: error === undefined ? undefined : "none",
+        }}
+      >
+        <div className="upc">
+          <svg ref={updateRef}></svg>
+        </div>
+        <div className="download">{downloadLink}</div>
+      </div>
     </div>
   );
 }
